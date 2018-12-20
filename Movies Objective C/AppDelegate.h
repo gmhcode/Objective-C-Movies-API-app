bd876bf019654b9bbd86808481ea7bb15eaf3568
GMHCode <greghughes988@gmail.com>
2018-12-20 15:23:54 -0700

    Initial Commit

---
+++ Movies Objective C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Movies Objective C
+//
+//  Created by Greg Hughes on 12/20/18.
+//  Copyright © 2018 Greg Hughes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Movies Objective C/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Movies Objective C
+//
+//  Created by Greg Hughes on 12/20/18.
+//  Copyright © 2018 Greg Hughes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
